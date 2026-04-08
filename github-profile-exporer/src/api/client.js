f2040cecaeffe2c61e89e69abecc492c728518c0
Bennah <bennah955@gmail.com>
2026-04-09 00:23:09 +0300

    Main project & main client(api call) setup

---
+++ github-profile-exporer/src/api/client.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "api.github.com",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem("Auth_token");
+  if(token){
+    config.headers.Authorization = `Bearer ${token} `
+  }
+  return config;
+})
+
+export default client;
